070196c250b30b84c162ebec742baac352f7de94
Bartosz Hanc <bartosz.hanc02@gmail.com>
2025-10-21 20:56:59 +0200

    update

--- lab01/tinny_gpt.py
+++ lab01/tinny_gpt.py
@@ -8,7 +8,6 @@ from typing import Optional
 import numpy as np
 from numpy.typing import NDArray, DTypeLike
 
-
 type FloatType = np.float32 | np.float64
 type BoolType = np.bool_
 type IntpType = np.intp
@@ -35,16 +34,16 @@ class Layer(ABC):
     = Layer(x; θ)`.
     """
 
-    x: Optional[NDArray[FloatType]]  # Reference to the inputs of the layer
-    y: Optional[NDArray[FloatType]]  # Reference to the outputs of the layer
+    x: Optional[NDArray]  # Reference to the inputs of the layer
+    y: Optional[NDArray]  # Reference to the outputs of the layer
 
     @abstractmethod
-    def parameters(self) -> list[NDArray[FloatType]]:
+    def parameters(self) -> list[NDArray]:
         """Return a list of references to the parameters of the layer."""
         raise NotImplementedError
 
     @abstractmethod
-    def gradients(self) -> list[Optional[NDArray[FloatType]]]:
+    def gradients(self) -> list[Optional[NDArray]]:
         """
         Return a list of references to the gradients ∂Loss/∂θ of the loss
         function w.r.t. the parameters, in the same order as the `.parameters()`
@@ -53,7 +52,7 @@ class Layer(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def forward(self, x: NDArray[FloatType], training: bool) -> NDArray[FloatType]:
+    def forward(self, x: NDArray, training: bool) -> NDArray:
         """
         Propagate the input `x` forward through the layer and return the output.
         Save the references to the input and output respectively in `self.x` and
@@ -62,7 +61,7 @@ class Layer(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def backward(self, grad_y: NDArray[FloatType]) -> NDArray[FloatType]:
+    def backward(self, grad_y: NDArray) -> NDArray:
         """
         Given ∂Loss/∂y (`grad_y`):
             * compute ∂Loss/∂x and ∂Loss/∂θ (where θ are the layer's params);
@@ -188,9 +187,9 @@ class Linear(Layer):
         self.y: Optional[NDArray[FloatType]] = None
 
         # Parameters and gradients
-        scale = xp.sqrt(4 / (self.vsize + self.hsize))
-        self.w: NDArray[FloatType] = xp.random.normal(0, scale, size=(self.vsize, self.hsize)).astype(self.dtype)
-        self.b: NDArray[FloatType] = xp.zeros(shape=(self.hsize,), dtype=self.dtype)
+        scale = xp.sqrt(4 / (vsize + hsize))
+        self.w: NDArray[FloatType] = xp.random.normal(0, scale, size=(vsize, hsize)).astype(dtype)
+        self.b: NDArray[FloatType] = xp.zeros(shape=(hsize,), dtype=dtype)
 
         self.grad_w: Optional[NDArray[FloatType]] = None
         self.grad_b: Optional[NDArray[FloatType]] = None
@@ -223,14 +222,8 @@ def softmax(x: NDArray[FloatType], axis: int) -> NDArray[FloatType]:
     return y / y.sum(axis=axis, keepdims=True)
 
 
-class MultiHeadSelfAttention(Layer):
-    def __init__(
-        self,
-        num_heads: int,
-        dim_model: int,
-        dropout: float,
-        dtype: DTypeLike = xp.float32,
-    ):
+class CausalMultiHeadSelfAttention(Layer):
+    def __init__(self, num_heads: int, dim_model: int, dropout: float, dtype: DTypeLike = xp.float32):
         assert dim_model % num_heads == 0
         assert 0.0 < dropout < 1.0
 
@@ -243,45 +236,55 @@ class MultiHeadSelfAttention(Layer):
         self.x: Optional[NDArray[FloatType]] = None
         self.y: Optional[NDArray[FloatType]] = None
 
+        self.q: Optional[NDArray[FloatType]] = None
+        self.k: Optional[NDArray[FloatType]] = None
+        self.v: Optional[NDArray[FloatType]] = None
+        self.scores: Optional[NDArray[FloatType]] = None
+
         # Parameters and gradients
         scale = xp.sqrt(4 / (self.dim_model + self.dim_head))
         size = (self.num_heads, self.dim_model, self.dim_head)
 
-        self.Wq: NDArray[FloatType] = xp.random.normal(0, scale, size).astype(self.dtype)
-        self.Wk: NDArray[FloatType] = xp.random.normal(0, scale, size).astype(self.dtype)
-        self.Wv: NDArray[FloatType] = xp.random.normal(0, scale, size).astype(self.dtype)
+        self.wq: NDArray[FloatType] = xp.random.normal(0, scale, size).astype(dtype)
+        self.wk: NDArray[FloatType] = xp.random.normal(0, scale, size).astype(dtype)
+        self.wv: NDArray[FloatType] = xp.random.normal(0, scale, size).astype(dtype)
 
         self.map_out = Sequential(Linear(dim_model, dim_model), Dropout(dropout))
 
-        self.grad_Wq: Optional[NDArray[FloatType]] = None
-        self.grad_Wk: Optional[NDArray[FloatType]] = None
-        self.grad_Wv: Optional[NDArray[FloatType]] = None
+        self.grad_wq: Optional[NDArray[FloatType]] = None
+        self.grad_wk: Optional[NDArray[FloatType]] = None
+        self.grad_wv: Optional[NDArray[FloatType]] = None
 
     def parameters(self) -> list[NDArray[FloatType]]:
-        return [self.Wq, self.Wk, self.Wv] + self.map_out.parameters()
+        return [self.wq, self.wk, self.wv] + self.map_out.parameters()
 
     def gradients(self) -> list[Optional[NDArray[FloatType]]]:
-        return [self.grad_Wq, self.grad_Wk, self.grad_Wv] + self.map_out.gradients()
+        return [self.grad_wq, self.grad_wk, self.grad_wv] + self.map_out.gradients()
 
     def forward(self, x: NDArray[FloatType], training: bool) -> NDArray[FloatType]:
-        assert x.ndim == 3
-        batch_size, seq_len, _ = x.shape
+        B, T, _ = x.shape
 
         # Shape `(batch_size, seq_len, dim_model)`
         self.x = x
 
         # Shape `(batch_size, 1, seq_len, dim_model)`
         x = x[:, None, :, :]
+
         # Shape `(batch_size, num_heads, seq_len, dim_head)`
-        Q, K, V = x @ self.Wq, x @ self.Wk, x @ self.Wv
+        self.q, self.k, self.v = x @ self.wq, x @ self.wk, x @ self.wv
+
         # Shape `(batch_size, num_heads, seq_len, seq_len)`
-        scores = softmax(Q @ K.transpose(0, 1, 3, 2) / self.dim_head**0.5, axis=-1)
+        self.scores = self.q @ self.k.transpose(0, 1, 3, 2) / self.dim_head**0.5
+        self.scores = self.scores + xp.triu(xp.full((T, T), -xp.inf), k=1)
+        self.scores = softmax(self.scores, axis=-1)  # type: ignore
+
         # Shape `(batch_size, num_heads, seq_len, dim_head)`
-        self.y = scores @ V
+        self.y = self.scores @ self.v
+
         # Shape `(batch_size, seq_len, num_heads, dim_head)`
         self.y = self.y.transpose(0, 2, 1, 3)
         # Shape `(batch_size, seq_len, dim_model)`
-        self.y = self.y.reshape(batch_size, seq_len, -1)
+        self.y = self.y.reshape(B, T, -1)
         # Shape `(batch_size, seq_len, dim_model)`
         self.y = self.map_out.forward(self.y, training)
 
@@ -295,7 +298,43 @@ class MultiHeadSelfAttention(Layer):
         grad_x = grad_x.transpose(0, 2, 1, 3)
 
 
-class Embedding(Layer): ...
+class Embedding(Layer):
+    def __init__(self, num_embeddings: int, dim_embedding: int, dtype: DTypeLike = xp.float32) -> None:
+        self.num_embeddings: int = num_embeddings
+        self.dim_embedding: int = dim_embedding
+        self.dtype: DTypeLike = dtype
+
+        # Input and output NDArrays
+        self.x: Optional[NDArray[IntpType]] = None
+        self.y: Optional[NDArray[FloatType]] = None
+
+        # Parameters and gradients
+        scale = xp.sqrt(2 / dim_embedding)
+        size = (num_embeddings, dim_embedding)
+        self.w: NDArray[FloatType] = xp.random.normal(0, scale, size).astype(dtype)
+        self.grad_w: Optional[NDArray[FloatType]] = None
+
+    def parameters(self) -> list[NDArray[FloatType]]:
+        return [self.w]
+
+    def gradients(self) -> list[Optional[NDArray[FloatType]]]:
+        return [self.grad_w]
+
+    def forward(self, x: NDArray[IntpType], training: bool) -> NDArray[FloatType]:
+        assert x.ndim == 2
+        # Inp shape `(batch_size, seq_len)`
+        # Out shape `(batch_size, seq_len, dim_embedding)`
+        self.x = x
+        self.y = self.w[x, :]
+        return self.y
+
+    def backward(self, grad_y: NDArray[FloatType]) -> None:  # type: ignore
+        assert self.x is not None
+        self.grad_w = xp.zeros_like(self.w)
+        xp.add.at(self.grad_w, self.x, grad_y)
+
+
+class PositionalEmbedding(Layer): ...
 
 
 class LayerNorm(Layer): ...
@@ -303,7 +342,7 @@ class LayerNorm(Layer): ...
 
 class Optimizer(ABC):
     # References to the parameters of the model
-    parameters: list[NDArray[FloatType]]
+    params: list[NDArray[FloatType]]
 
     @abstractmethod
     def __init__(self, parameters: list[NDArray[FloatType]], *args, **kwargs):
@@ -319,15 +358,17 @@ class Optimizer(ABC):
         raise NotImplementedError
 
 
-class Adam(Optimizer):
+class AdamW(Optimizer):
     def __init__(
         self,
         params: list[NDArray[FloatType]],
         lr: float,
+        weight_decay: float,
         betas: tuple[float, float] = (0.9, 0.999),
         eps: float = 1e-8,
     ):
         self.lr: float = lr
+        self.weight_decay: float = weight_decay
         self.eps: float = eps
         self.beta1: float = betas[0]
         self.beta2: float = betas[1]
@@ -348,4 +389,10 @@ class Adam(Optimizer):
             mhat_p = m_p / (1 - self.beta1**self.t)
             vhat_p = v_p / (1 - self.beta2**self.t)
 
+            p *= 1 - self.lr * self.weight_decay
             p -= self.lr * mhat_p / (self.eps + vhat_p**0.5)
+
+
+# =================================================================================================
+# =================================================================================================
+# =================================================================================================
